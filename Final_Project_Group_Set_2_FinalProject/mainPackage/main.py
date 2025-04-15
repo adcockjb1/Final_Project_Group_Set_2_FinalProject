a2d49137b93cef46836f65297cad35eeee2cb8a8
adcockjb1 <adcoc@DESKTOP-K2GDQGK>
2025-04-15 13:43:22 -0400

    Add project files.

---
+++ Final_Project_Group_Set_2_FinalProject/mainPackage/main.py
@@ -0,0 +1,21 @@
+# File Name : main.py
+# Group name : Final Project Group Set 2
+# Student Name: Joseph Adcock
+#               Kengo Ishizuka
+#               Omar Alkhawaga
+# email: adcockjb@mail.uc.edu
+#        ishizuko@mail.uc.edu
+#        alkhawoe@mail.uc.edu
+# Assignment Number: Final Project
+# Due Date:   04/30/2025
+# Course #/Section: IS 4010-001
+# Semester/Year: Spring 2025
+# Brief Description of the assignment: Solves a scavenger hunt and provides picture proof that we were there
+
+
+# Brief Description of what this module does: 
+# Citations: 
+
+# Anything else thats relevant:
+
+if __name__ == "__main__":
